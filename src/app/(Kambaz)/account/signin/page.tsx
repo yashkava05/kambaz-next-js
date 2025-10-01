47120d980fa48b5e9325d6da15d64af9506e19c2
yashkava05 <110620608+yashkava05@users.noreply.github.com>
2025-10-01 13:26:52 -0400

    web dev one

--- src/app/(Kambaz)/account/signin/page.tsx
+++ src/app/(Kambaz)/account/signin/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 export default function Signin() {
  return (
    <div id="wd-signin-screen">
-     <h3>SIGN IN PAGE - YASH KAVA</h3>
+    <h3>Assignment One - Yash Kava</h3>
+     <h3>SIGN IN PAGE</h3>
      <input className="wd-username" placeholder="username" /> <br />
       <input className="wd-password" placeholder="password" type="password" /> <br />
       <Link id="wd-signin-btn" href="/Dashboard"> Sign In </Link> <br />
