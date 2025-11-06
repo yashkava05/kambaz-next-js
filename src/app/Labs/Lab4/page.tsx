514441e1969eb14ac9ac968d92122b84c8ce165c
yashkava05 <110620608+yashkava05@users.noreply.github.com>
2025-11-06 15:55:31 -0500

    bugs solved

--- src/app/Labs/Lab4/page.tsx
+++ src/app/Labs/Lab4/page.tsx
@@ -13,7 +13,7 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
-import ReduxExamples from "./ReduxExamples/page";
+import ReduxExamples from "./ReduxExamples"; // Changed from ./ReduxExamples/page
 
 export default function Lab4() {
   function sayHello() {
