1b9ee24536e47a03b816bad58a15d0a214ad27b8
yashkava05 <110620608+yashkava05@users.noreply.github.com>
2025-12-10 18:21:03 -0500

    code done

--- src/app/Labs/page.tsx
+++ src/app/Labs/page.tsx
@@ -5,7 +5,7 @@ export default function Labs() {
   return (
     <div id="wd-labs">
      <h1>Labs</h1>
-     <h2>Harshil Parmar</h2>
+     <h2>Yash Kava</h2>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
